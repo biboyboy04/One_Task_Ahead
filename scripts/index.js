4f495e449f41dd8054b0807aec5af1b2d0cf4175
biboyboy04 <biboyboy04@gmail.com>
2023-04-25 15:54:30 +0800

    Refactor
    Rename index.html to board.html
    
    Add link to Category cards

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,9 @@ import "./pomodoro.js";
 dragAndDrop();
 
 const todoLane = document.getElementById("todo-lane");
+const doingLane = document.getElementById("doing-lane");
+const doneLane = document.getElementById("done-lane");
+
 const todoButton = document.getElementById("todo-submit");
 const newTask = todoModule.createTask("Eat", "");
 const newTask1 = todoModule.createTask("Eat1", "");
@@ -17,6 +20,13 @@ const newTask2 = todoModule.createTask("Eat2", "");
 todoLane.insertBefore(newTask, todoButton);
 todoLane.insertBefore(newTask1, todoButton);
 todoLane.insertBefore(newTask2, todoButton);
+
+const newTask3 = todoModule.createTask("Eat2", "");
+const newTask4 = todoModule.createTask("Eat2", "");
+const newTask5 = todoModule.createTask("Eat2", "");
+doingLane.appendChild(newTask3);
+doingLane.appendChild(newTask4);
+doingLane.appendChild(newTask5);
 
 // PUT INTO A MODULE
 const openModalBtn = document.querySelector("#todo-submit");
@@ -45,3 +55,25 @@ myForm.addEventListener("submit", (e) => {
   // handle form submission here
   closeModal();
 });
+
+// Architecture
+// To Do
+
+// Design Concept
+// Design Objectives
+// Design Goals
+// Design Philosophy
+
+// Daily Routine
+// To Do
+// Morning
+
+// Afternoon
+
+// Evening
+
+// School Subjects
+// Assessment
+// Essay
+// Quiz
+// Powerpoint
